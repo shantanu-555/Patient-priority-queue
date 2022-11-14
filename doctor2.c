e45bb56ce0e5f8227507d01303996132fc74614f
Shantanu Motiani <shantanumotiani@gmail.com>
2022-11-15 00:43:13 +0100

    Final touch ups, all tests passed!

--- doctor2.c
+++ doctor2.c
@@ -1,18 +1,18 @@
+#include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <assert.h>
 
-// Node
-typedef struct node {
-    char name[201];
-    int age;
-    // Higher values indicate higher priority
-    int priority;
-    struct node* next;
+// Patient Node
+typedef struct Node {
+  char name[201];
+  int age;
+  // Higher values indicate higher priority
+  int priority;
+  struct Node* next;
 } Node;
- 
-// Function to Create A New Node
+
+// Function to Create A New Patient Node
 Node* newNode(char name[201], int age, int pri) {
   Node* temp = (Node*)malloc(sizeof(Node));
   strcpy(temp->name, name);
@@ -32,26 +32,25 @@ int peek_priority(Node** head) {
   return (*head)->priority;
 }
 
-// Function to remove the element with the highest priority from the list
-void pop(Node** head) {
+// Function to remove the node with the highest priority from the list
+void treatPatient(Node** head) {
   Node* temp = *head;
   (*head) = (*head)->next;
   free(temp);
 }
- 
-// Function to push new node into the list according to priority
-void push(Node** head, char name[201], int age, int pri) {
+
+// Function to add new Patient Node into the list according to priority
+void newPatient(Node** head, char name[201], int age, int pri) {
   Node* start = (*head);
   // Create new Node
   Node* temp = newNode(name, age, pri);
-  // Special Case: The head of list has lesser priority than new node. 
-  // So insert new node before head node and change head node.
+  // If the head of the list has lesser priority than new node,
+  // insert new node before head node and change head node.
   if ((*head)->priority < pri) {
     // Insert New Node before head
     temp->next = *head;
     (*head) = temp;
-  }
-  else {
+  } else {
     // Traverse the list and find a position to insert new node
     while (start->next != NULL && start->next->priority > pri) {
       start = start->next;
@@ -65,21 +64,21 @@ void push(Node** head, char name[201], int age, int pri) {
 // Function that generates a list of patients
 void list(Node* head) {
   if (head->next != NULL) {
-    printf("%s (age: %d, priority: %d)\n", head->name, peek_age(&head), peek_priority(&head));
+    printf("%s (age: %d, priority: %d)\n", head->name, peek_age(&head),
+           peek_priority(&head));
     list(head->next);
   }
 }
- 
+
 // Function to check if list is empty
 int isEmpty(Node** head) {
   return (*head) == NULL;
 }
 
 int main() {
-
   int count = 0;
   char command;
-  Node* patients = newNode("X", 0, -1); 
+  Node* patients = newNode("X", 0, -1);
 
   while (1) {
     printf("Currently there are %d patients in the queue.\n", count);
@@ -89,33 +88,34 @@ int main() {
       printf("What do you want to do? [N]ew patient, [T]reat patient, [L]ist, [Q]uit:\n");
     }
     scanf("%c", &command);
-    getchar();  
+    getchar();
 
-    // Creating new patient and adding into list by priority
+    // Creating new patient and adding node into list by priority
     if (command == 'N') {
       char name[201];
       printf("What is the name of the patient?\n");
       scanf("%200[^\n]", name);
-      getchar(); 
+      getchar();
 
       int age;
       printf("What is the age of the patient?\n");
       scanf("%d", &age);
-      getchar(); 
-      
+      getchar();
+
       int priority;
       printf("What is the priority of the patient?\n");
       scanf("%d", &priority);
-      getchar(); 
- 
-      push(&patients, name, age, priority);  
+      getchar();
+
+      newPatient(&patients, name, age, priority);
       count++;
     }
 
-    // Treating the patient and removing from the list
+    // Treating the patient and removing node from the list
     if (command == 'T') {
-      printf("Treating the patient %s (age: %d, priority: %d).\n", patients->name, peek_age(&patients), peek_priority(&patients));
-      pop(&patients);
+      printf("Treating the patient %s (age: %d, priority: %d).\n",
+             patients->name, peek_age(&patients), peek_priority(&patients));
+      treatPatient(&patients);
       count--;
     }
 
@@ -124,9 +124,10 @@ int main() {
       list(patients);
     }
 
-    // Quitting thr program
+    // Quitting the program
     if (command == 'Q') {
       printf("Goodbye!\n");
+      free(patients);
       exit(0);
     }
   }
